86514f4ed17a662b4fcd303529fc868a72e443bf
bainss07 <t.bains046@mybvc.ca>
2025-04-11 17:52:09 -0600

    update frontend deployment link

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:8081', 'https://love-is-free--ylz6bpd06y.expo.app'], // Explicitly allow frontend origins
+  origin: ['http://localhost:8081', 'https://love-is-free--6rsi9pi4za.expo.app'], // Explicitly allow frontend origins
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
